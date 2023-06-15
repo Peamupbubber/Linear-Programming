41a0e9f6fba53b7e0ed06da15c375ef34bdc02b3
Hunter <hunteralansmith@outlook.com>
2023-06-14 21:22:06 -0700

    new files

---
+++ LP.java
@@ -0,0 +1,112 @@
+import java.util.Scanner;
+import java.io.File;
+import java.util.List;
+import java.util.ArrayList;
+
+enum Type {
+    VARIABLE,
+    CONSTRAINT,
+    OF_MIN,
+    OF_MAX,
+    ERROR
+}
+
+class Constraint {
+    LinearSum left;
+    LinearSum right;
+    String op;
+
+    public Constraint(String con) {
+        left = new LinearSum();
+        right = new LinearSum();
+        parseConstraint(con);
+    }
+
+    private void parseConstraint(String con) {
+        String[] items = con.split(" ");
+
+        for(int i = 0; i < items.length; i++) {
+            int j = 0;
+            while(j < items[i].length() && constChar(items[i].charAt(j))) {
+                j++;
+            }
+
+        }
+    }
+
+    private boolean constChar(char c) {
+        return c == '(' || c == ')' || c == '.' || (c >= '0' && c <= '9');
+    }
+}
+
+public class LP {
+    private List<String> variables;
+    private List<String> constraints;
+    private String objectiveFunction;
+
+    private List<String> variableIDs;
+    private List<String> constraintIDs;
+    private List<String> ofMinIDs;
+    private List<String> ofMaxIDs;
+
+    public LP(String file) throws Exception {
+        variables = new ArrayList<String>();
+        constraints = new ArrayList<String>();
+        objectiveFunction = null;
+
+        initializeIDNames();
+        parse(file);
+    }
+
+    private void initializeIDNames() {
+        variableIDs = new ArrayList<String>();
+        constraintIDs = new ArrayList<String>();
+        ofMinIDs = new ArrayList<String>();
+        ofMaxIDs = new ArrayList<String>();
+
+        variableIDs.add("var");
+        variableIDs.add("variable");
+
+        constraintIDs.add("st");
+        constraintIDs.add("s.t.");
+        constraintIDs.add("subject to");
+
+        ofMinIDs.add("min");
+        ofMinIDs.add("minimize");
+
+        ofMaxIDs.add("max");
+        ofMaxIDs.add("maximize");
+    }
+
+    private void parse(String fileName) throws Exception {
+        File file = new File(fileName);
+        try {
+            Scanner scanner = new Scanner(file);
+            scanner.useDelimiter(";");
+    
+            while(scanner.hasNext()) {
+                String next = scanner.next();
+                System.out.print(next);
+            }
+            scanner.close();
+        }
+        catch(Exception e) {
+            System.out.println("Invalid file: " + fileName);
+        }
+    }
+
+    Type type(String identifier) {
+        identifier = identifier.toLowerCase();
+
+        if(variableIDs.contains(identifier))
+            return Type.VARIABLE;
+        else if(constraintIDs.contains(identifier))
+            return Type.CONSTRAINT;
+        else if(ofMinIDs.contains(identifier))
+            return Type.OF_MIN;
+        else if(ofMaxIDs.contains(identifier))
+            return Type.OF_MAX;
+        else
+            return Type.ERROR;
+    }
+}
---
+++ LinearSum.java
@@ -0,0 +1,39 @@
+/* LE = CONSTANT * VARIABLE OP LE
+ * ex: 2x - 3y - 4
+ *     2, x, (-3, y, (4, null, (null)))
+ *      
+ */ 
+
+public class LinearSum {
+    private float constant;
+    private String variable;
+
+    private LinearSum next;
+
+    public LinearSum() {
+
+    }
+
+    //Takes in a string and sets the constant to a float, can be given as "(x/y)", "x.y", or "x"
+    public void setConstant(String constant) {
+        String[] frac = constant.split("/");
+        if(frac.length == 2) {
+            this.constant = Integer.parseInt(frac[0].substring(1)) / (float)Integer.parseInt(frac[1].substring(0, frac[1].length() - 1));
+        }
+        else {
+            this.constant = Float.parseFloat(constant);
+        }
+    }
+
+    public void setVaraible(String variable) {
+        this.variable = variable;
+    }
+
+    public void setNext(LinearSum next) {
+        this.next = next;
+    }
+
+    public float getConstant() {
+        return constant;
+    }
+}
